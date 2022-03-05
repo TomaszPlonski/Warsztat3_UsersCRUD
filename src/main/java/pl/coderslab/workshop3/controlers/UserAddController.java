a0cc44aa24600daa3f1d3f11bd1f1e0984930c03
Tomasz Płoński <tom.plonski@gmail.com>
2022-03-05 10:04:48 +0100

    add user Servlet ready

--- src/main/java/pl/coderslab/workshop3/controlers/UserAddController.java
+++ src/main/java/pl/coderslab/workshop3/controlers/UserAddController.java
@@ -1,4 +1,29 @@
 package pl.coderslab.workshop3.controlers;
 
-public class UserAddController {
+import pl.coderslab.workshop3.domain.dao.UserDao;
+import pl.coderslab.workshop3.domain.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/add")
+public class UserAddController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            getServletContext().getRequestDispatcher("/WEB-INF/views/add.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("userName");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        User newUser = UserDao.createUser(new User(userName,email,password));
+
+        resp.sendRedirect("/list");
+    }
 }
