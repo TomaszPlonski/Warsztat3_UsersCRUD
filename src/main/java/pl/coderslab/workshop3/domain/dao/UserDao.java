daa120330e13a3a01972cad87632c956b4a64af0
Tomasz Płoński <tom.plonski@gmail.com>
2022-03-06 15:17:22 +0100

    fixed bug with update -> username switched with email

--- src/main/java/pl/coderslab/workshop3/domain/dao/UserDao.java
+++ src/main/java/pl/coderslab/workshop3/domain/dao/UserDao.java
@@ -12,7 +12,7 @@ public class UserDao {
 
     private static final String CREATE_USER_QUERY = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
     private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
-    private static final String UPDATE_QUERY = "UPDATE users SET email = ?, username = ?, password = ? WHERE id = ?";
+    private static final String UPDATE_QUERY = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
     private static final String DELETE_QUERY = "DELETE FROM users WHERE id = ?";
     private static final String FIND_ALL_QUERY = "SELECT * FROM users";
 
